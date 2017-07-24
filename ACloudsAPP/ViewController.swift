8d0cd2ab640872a948b1c0aaf49f2d38ed44288d
Terence Chen <terencechen55@gmail.com>
2017-07-24 13:56:26 +0800

    網路傳輸套件，初步整合

--- ACloudsAPP/ViewController.swift
+++ ACloudsAPP/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
-
+import Alamofire
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        Alamofire.request("https://httpbin.org/get").responseJSON { (response) in
+            print(response.request! as Any)
+            print(response.response! as Any)
+            print(response.data! as Any)
+            print(response.result)
+        }
     }
 
     override func didReceiveMemoryWarning() {
