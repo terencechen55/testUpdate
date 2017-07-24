8f85032e80c1739d386b86025df6d925a8ff6070
Terence Chen <terencechen55@gmail.com>
2017-07-24 11:15:47 +0800

    Init

---
+++ ACloudsAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ACloudsAPP
+//
+//  Created by 泰倫斯 on 2017/7/24.
+//  Copyright © 2017年 iOSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
